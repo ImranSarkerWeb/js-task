21badaebd08fbae013f1f7738fd4d79eaab3182c
Imransarkerweb <imransarker.web@gmail.com>
2023-02-12 07:45:24 +0600

    add newtask

--- main.js
+++ main.js
@@ -2,19 +2,6 @@ const plus =document.getElementById('plus')
 const minus =document.getElementById('minus')
 const counter = document.getElementById('counter')
 
-//these variable works for to do list
-
-const newtask =document.getElementById('newtask')
-const addItemButton = document.getElementById('addItem')
-const todoUl = document.querySelector('#items')
-const completeUL = document.querySelector('complete-list ul')
-
-
-
-const taskList = document.getElementById('task')
-
-
-const removeItem = document.querySelector('.remove')
 let value1 = 0;
 
 counter.innerText = value1;
@@ -29,23 +16,34 @@ minus.addEventListener('click', function(){
     counter.innerText = value1;
 })
 
+//these variable works for to do list
+
+const newtask =document.getElementById('newtask')
+const addItemButton = document.getElementById('addItem')
+const todoUl = document.querySelector('#items')
+const completeUL = document.querySelector('complete-list ul')
+
+
+//functions
+function createTask(task){
+    const item = document.createElement('li')
+    item.setAttribute('class', 'item')
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    const label = document.createElement('label')
+    label.innerText = task.value;
+
+    item.appendChild(checkbox)
+    item.appendChild(label)
+    return item;
+
+}
+
+addItemButton.addEventListener('click', function(){
+    newtask.value ? todoUl.appendChild(createTask(newtask)):alert('please type your work!')
+    newtask.value = ""
+})
+
+
 
 //below event will add something my todo list
-
-addItem.addEventListener('click', function(){
-    const li = document.createElement('li');
-    li.innerText = newtask.value;
-    newtask.value ? taskList.appendChild(li):alert("Please input something first!")
-    newtask.value = '';
-})
-
-const taskItem = document.querySelector('.taskItem').parentNode;
-console.log(taskItem)
-
-taskList.addEventListener('click', function(event){
-
-    console.log(event.target)
-    event.target.parentNode.removeChild(event.target)
-})
-
-
